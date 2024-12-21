22ecda1b2ae78bd7d034dc798ecb5df4e9b480c1
SKhairulin <skhairulin@m.ru>
2024-12-21 14:43:19 +0300

    fix auth

--- src/components/contentMainPage/block-7.jsx
+++ src/components/contentMainPage/block-7.jsx
@@ -10,7 +10,7 @@ export const Block7 = () => {
             <div className="u-align-center u-border-2 u-border-palette-2-base u-container-align-center u-container-style u-list-item u-repeater-item u-shape-rectangle u-white u-list-item-1" data-animation-name="customAnimationIn" data-animation-duration="1750" data-animation-delay="250">
               <div className="u-container-layout u-similar-container u-valign-top u-container-layout-1">
                 <h3 className="u-custom-font u-font-montserrat u-text u-text-3">Standart</h3>
-                <p className="u-file-icon u-text u-text-4">Неограниченные занятия <br/>Бассейн<br/>Сауна<br/>Персональный шкафчик<br/>
+                <p className="u-file-icon u-text u-text-4">Неограниченные занятия <br/>Бассейн<br/>Сауна<br/>Персональный шкафчик<br/><br/>
                 </p>
                 <a href="#" className="u-active-white u-border-2 u-border-active-grey-50 u-border-hover-grey-50 u-border-palette-2-base u-btn u-button-style u-custom-font u-font-montserrat u-hover-white u-palette-2-base u-text-body-color u-btn-1"> Запросить тарифы</a>
               </div>
@@ -20,15 +20,15 @@ export const Block7 = () => {
                 <h3 className="u-custom-font u-font-montserrat u-text u-text-5">Comfort</h3>
                 <p className="u-file-icon u-text u-text-6"> Услуги пакета Standart<br/>Услуги прачечной <br/>Массажная терапия<br/>Составление диеты<br/>Составление графика тренировок
                 </p>
-                <a href="#" className="u-active-white u-border-2 u-border-active-grey-50 u-border-hover-grey-50 u-border-palette-2-base u-btn u-button-style u-custom-font u-font-montserrat u-hover-white u-palette-2-base u-text-body-color u-btn-2"> Запросить тарифы</a>
+                <a href="#" className="u-active-white u-border-2 u-border-active-grey-50 u-border-hover-grey-50 u-border-palette-2-base u-btn u-button-style u-custom-font u-font-montserrat u-hover-white u-palette-2-base u-text-body-color u-btn-2">Запросить тарифы</a>
               </div>
             </div>
             <div className="u-align-center u-border-2 u-border-palette-2-base u-container-align-center u-container-style u-list-item u-repeater-item u-shape-rectangle u-white u-list-item-3" data-animation-name="customAnimationIn" data-animation-duration="1750" data-animation-delay="250">
               <div className="u-container-layout u-similar-container u-valign-top u-container-layout-3">
                 <h3 className="u-custom-font u-font-montserrat u-text u-text-7">Best</h3>
-                <p className="u-file-icon u-text u-text-8"> Услуги пакета Comfort<br/>Персональный тренер<br/>Персональный диетолог<br/>
+                <p className="u-file-icon u-text u-text-8"> Услуги пакета Comfort<br/>Персональный тренер<br/>Персональный диетолог<br/><br/><br/>
                 </p>
-                <a href="#" className="u-active-white u-border-2 u-border-active-grey-50 u-border-hover-grey-50 u-border-palette-2-base u-btn u-button-style u-custom-font u-font-montserrat u-hover-white u-palette-2-base u-text-body-color u-btn-3"> Учить больше</a>
+                <a href="#" className="u-active-white u-border-2 u-border-active-grey-50 u-border-hover-grey-50 u-border-palette-2-base u-btn u-button-style u-custom-font u-font-montserrat u-hover-white u-palette-2-base u-text-body-color u-btn-3">Запросить тарифы</a>
               </div>
             </div>
           </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,91 +1,109 @@
-import { useEffect, useState } from 'react';
-
+import { useContext, useEffect, useState } from 'react';
+import { Context } from '../index'
 import { ModalLogReg } from '../ui/modal/ModalLogReg';
 import { typeLog, typeReg } from '../config/consts';
 import { login, reg } from '../api/authAPI';
 import { Notification } from '../ui/Notific/Notification';
 
 export const Header = () => {
+    const { user } = useContext(Context)
     const [showModal, setShowModal] = useState(false)
     const [typeWindow, setTypeWindow] = useState('')
-    const [data, setData] = useState({username:'', email:'', password:''})
-    const [notif, setNotif] = useState({visible:false, message:''})
+    const [data, setData] = useState({ username: '', email: '', password: '' })
+    const [notif, setNotif] = useState({ visible: false, message: '' })
 
     const handleChange = (event) => {
         const { name, value } = event.target;
-    
+
         setData(prev => ({
-          ...prev,
-          [name]: value
+            ...prev,
+            [name]: value
         }));
-      };
+    };
 
     const logRegAction = () => {
-        if(typeWindow===typeLog)
-            login({username:data.username, password: data.password}).then(ans=>setNotif(prev=>{return {visible:true, message:'Успешно авторизован'}}))
-                                                                    .catch(ans=>setNotif(prev=>{return {visible:true, message:'Не удалось авторизоваться'}}))
-        
-        if(typeWindow===typeReg)
-            reg({username:data.username, password:data.password, email:data.email}).then(ans=>setNotif(prev=>{return {visible:true, message:'Регистрация прошла успешно'}}))
-                                                                                    .catch(ans=>setNotif(prev=>{return {visible:true, message:'Не удалось зарегистрироваться'}}))
-        
+        if (typeWindow === typeLog)
+            login({ username: data.username, password: data.password }).then(ans => {
+                setNotif(prev => { return { visible: true, message: 'Успешно авторизован' } })
+                user.setIsAuth(true)
+                user.setUserName(data.username)
+                setShowModal(false)
+            })
+                .catch(ans => setNotif(prev => { return { visible: true, message: 'Не удалось авторизоваться' } }))
+
+        if (typeWindow === typeReg)
+            reg({ username: data.username, password: data.password, email: data.email }).then(ans => {
+                setNotif(prev => { return { visible: true, message: 'Регистрация прошла успешно' } })
+                setShowModal(false)
+            })
+                .catch(ans => setNotif(prev => { return { visible: true, message: 'Не удалось зарегистрироваться' } }))
+
     }
 
     return (
         <>
-            <header className="u-clearfix u-header u-sticky u-sticky-08cb u-header" id="sec-0ba6" style={{position: 'relative', margin: 0, justifyContent:'center'}}>
-            <div className="" style={{alignItems:'center', width: '100%', display: 'flex', justifyContent: 'space-around', padding: '10px 0'}}>
-                <div>
-                <a href="#" className="u-image u-logo" style={{display:'flex'}}>
-                    <img src="images/default-logo.png" className="u-logo-image u-logo-image-1" />
-                </a>
-                </div>
-                <div>
-                <nav className="">
-                    <div className="menu-collapse" style={{fontSize: '1rem', letterSpacing: '0px'}}>
-                        <a className="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-hamburger-link u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+            <header className="u-clearfix u-header u-sticky u-sticky-08cb u-header" id="sec-0ba6" style={{ position: 'relative', margin: 0, justifyContent: 'center' }}>
+                <div className="" style={{ alignItems: 'center', width: '100%', display: 'flex', justifyContent: 'space-around', padding: '10px 0' }}>
+                    <div>
+                        <a href="#" className="u-image u-logo" style={{ display: 'flex' }}>
+                            <img src="images/default-logo.png" className="u-logo-image u-logo-image-1" />
                         </a>
                     </div>
-                    <div className="u-nav-container">
-                        <ul className="u-nav u-unstyled u-nav-1"><li className="u-nav-item">
-                        </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="./" style={{padding: '10px 20px'}}>Главная</a>
-                            </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="" style={{padding: '10px 20px'}}>Лендинг</a>
-                            </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="" style={{padding: '10px 20px'}}>Контакты</a>
-                            </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="" style={{padding: '10px 20px'}}>О нас</a>
-                            </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="" style={{padding: '10px 20px'}}>Команда</a>
-                            </li>
-                            </ul>
-                            
-                    </div>
-                    
-                    <div className="u-nav-container-collapse">
-                        <div className="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
-                            <div className="u-inner-container-layout u-sidenav-overflow">
-                                <div className="u-menu-close"></div>
-                                <ul className="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">Страница 1</a>
-                                </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="./">Главная</a>
-                                    </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">Лендинг</a>
-                                    </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">Контакты</a>
-                                    </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">О нас</a>
-                                    </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">Команда</a>
-                                    </li></ul>
+                    <div>
+                        <nav className="">
+                            <div className="menu-collapse" style={{ fontSize: '1rem', letterSpacing: '0px' }}>
+                                <a className="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-hamburger-link u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+                                </a>
                             </div>
+                            <div className="u-nav-container">
+                                <ul className="u-nav u-unstyled u-nav-1"><li className="u-nav-item">
+                                </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="./" style={{ padding: '10px 20px' }}>Главная</a>
+                                    </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="" style={{ padding: '10px 20px' }}>Лендинг</a>
+                                    </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="" style={{ padding: '10px 20px' }}>Контакты</a>
+                                    </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="" style={{ padding: '10px 20px' }}>О нас</a>
+                                    </li><li className="u-nav-item"><a className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="" style={{ padding: '10px 20px' }}>Команда</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+
+                            <div className="u-nav-container-collapse">
+                                <div className="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+                                    <div className="u-inner-container-layout u-sidenav-overflow">
+                                        <div className="u-menu-close"></div>
+                                        <ul className="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">Страница 1</a>
+                                        </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="./">Главная</a>
+                                            </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">Лендинг</a>
+                                            </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">Контакты</a>
+                                            </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">О нас</a>
+                                            </li><li className="u-nav-item"><a className="u-button-style u-nav-link" href="">Команда</a>
+                                            </li></ul>
+                                    </div>
+                                </div>
+                                <div className="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+                            </div>
+                        </nav>
+                    </div>
+                    <div>
+                        {user.isAuth ? 
+                        <div style={{display:'flex', alignContent:'center'}}>
+                        Добро пожаловать, {user.userName}
+                        <img src='images/exit.png' style={{marginLeft:'30px',width:'17px', height:'17px', cursor:'pointer'}}/>
                         </div>
-                        <div className="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+                        :
+                        <div>
+                        <div className='u-nav-item'><a onClick={() => { setTypeWindow(typeLog); setShowModal(true) }} className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style={{ marginRight: '20px', cursor: 'pointer' }} >Войти</a></div>
+                        <div className='u-nav-item'><a onClick={() => { setTypeWindow(typeReg); setShowModal(true) }} className="u-nav-item u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style={{ cursor: 'pointer' }}>Регистрация</a>
+                        </div>
+                        </div>
+                        }
+                        
                     </div>
-                </nav>
-                </div>
-                <div>
-                    <div className='u-nav-item'><a onClick={()=>{setTypeWindow(typeLog);setShowModal(true)}} className="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style={{marginRight:'20px', cursor:'pointer'}} >Войти</a></div>
-                            
-                    <div className='u-nav-item'><a onClick={()=>{setTypeWindow(typeReg);setShowModal(true)}} className="u-nav-item u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style={{cursor:'pointer'}}>Регистрация</a>
-                    </div>
-                    </div>
-            </div></header>
+                </div></header>
 
             <ModalLogReg
                 show={showModal}
-                onHide={()=>setShowModal(false)}
+                onHide={() => setShowModal(false)}
                 type={typeWindow}
                 changeType={setTypeWindow}
                 actionBtn={logRegAction}
@@ -93,7 +111,7 @@ export const Header = () => {
                 handleChange={handleChange}
             />
 
-            <Notification message={notif.message} visible={notif.visible} duration={2000} setVis={vis=> setNotif(prev=>prev.visible=vis)}/>
+            <Notification message={notif.message} visible={notif.visible} duration={2000} setVis={vis => setNotif(prev => prev.visible = vis)} />
         </>
     )
 }
